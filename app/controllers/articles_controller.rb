77d4c6476eae631e3b5c94b029a670613e59fa5f
ranjanPrZ <ranjantabby@gmail.com>
2018-05-26 15:01:47 +0530

    destroy method added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,10 @@
 class ArticlesController < ApplicationController
+  
+  before_action :set_article, only: [:edit,:update,:show,:destroy]
+
+	def index
+		@articles =Article.all
+	end
 
 	def new
 		@article = Article.new
@@ -9,6 +15,7 @@ class ArticlesController < ApplicationController
 		#render plain: params[:article].inspect
 		@article =Article.new(article_params)
 		if @article.save
+			flash[:notice] = "Article was successfully created"
 			redirect_to @article
 		else
 			render "new"
@@ -16,12 +23,36 @@ class ArticlesController < ApplicationController
 	end
 
 	def show
-	  @article = Article.all
+	  
 
 	end
 
+	def edit
+	 
+
+	end
+
+	def update
+				if @article.update(article_params)
+			flash[:notice]="Article was successfully updated"
+			redirect_to @article
+		else
+			render "edit"
+		end
+	end
+
+	def destroy
+	  
+	  @article.destroy
+	  flash[:notice]="Article was successfully deleted!"
+	  redirect_to articles_path
+	end
 
 	private
+	  def set_article
+	  	@article =Article.find(params[:id])
+	  end
+
 	  def article_params
 	  	params.require(:article).permit(:title,:description)
 
